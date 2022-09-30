35c2d021fc1d44da0bfef5ba544c9d07d51a897c
Jacob Long <jclong98@gmail.com>
2022-09-29 19:24:14 -0700

    feat: global component types

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,1 +1,10 @@
+
+import '@vue/runtime-core'
+
 export { default as JBtn } from './JBtn.vue'
+
+declare module '@vue/runtime-core' {
+    export interface GlobalComponents {
+        JBtn: typeof import('./components/JBtn.vue')['default']
+    }
+}
