d45b636015ebc89fbd5bae6627b863f0e17995f8
Jacob Long <jclong98@gmail.com>
2022-10-02 15:31:51 -0700

    feat: add scripts to generate components and composables

---
+++ packages/ui/scripts/new-component.js
@@ -0,0 +1,95 @@
+import fs from "fs"
+import path from "path"
+
+// get command line arguments to create a new component
+const args = process.argv.slice(2)
+let componentName = args[0]
+
+if (!componentName) {
+  console.error("Please provide a component name")
+  process.exit(1)
+}
+
+// capitalize the first letter of the component
+// name just in case it's not capitalized already
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+componentName = capitalizeFirstLetter(componentName)
+
+// get the current working directory
+const cwd = process.cwd()
+
+// get the path to the components folder
+const componentsPath = path.join(cwd, "src", "components")
+
+// get the path to the new component folder
+const componentFolderPath = path.join(componentsPath, componentName)
+
+// create the new component folder
+fs.mkdirSync(componentFolderPath)
+
+// get the path to the new component
+const componentPath = path.join(componentFolderPath, `${componentName}.vue`)
+
+// get the path to the new component's test
+const testPath = path.join(componentFolderPath, `${componentName}.test.ts`)
+
+const indexPath = path.join(componentFolderPath, "index.ts")
+
+console.log({
+  cwd,
+  componentFolderPath,
+  componentPath,
+  testPath,
+})
+
+// create the component
+const componentTemplate = `<script setup lang="ts">
+
+</script>
+
+<template>
+  <div></div>
+</template>
+`
+
+// create the component's test
+const testTemplate = `import { render } from '@testing-library/vue'
+
+import ${componentName} from './'
+
+describe('${componentName}.vue', () => {
+    it('renders component', () => { 
+        const { container } = render(${componentName})
+
+        expect(container).toMatchSnapshot()
+    })
+})
+`
+
+// create the component's index file
+const indexContent = `export { default as ${componentName} } from './${componentName}.vue'`
+
+const files = [
+  {
+    path: componentPath,
+    content: componentTemplate,
+  },
+  {
+    path: testPath,
+    content: testTemplate,
+  },
+  {
+    path: indexPath,
+    content: indexContent,
+  },
+]
+
+for (const file of files) {
+  fs.writeFile(file.path, file.content, function (err) {
+    if (err) throw err
+  })
+}
+
+console.log("Component created successfully")
---
+++ packages/ui/scripts/new-composable.js
@@ -0,0 +1,58 @@
+import fs from "fs"
+import path from "path"
+
+// get command line arguments to create a new composable
+const args = process.argv.slice(2)
+let composableName = args[0]
+
+if (!composableName) {
+  console.error("Please provide a composable name")
+  process.exit(1)
+}
+
+// make sure the composable starts with use
+if (!composableName.startsWith("use")) {
+  composableName = "use" + composableName
+}
+
+// get the current working directory
+const cwd = process.cwd()
+
+// get the path to the composables folder
+const composablesPath = path.join(cwd, "src", "composables")
+
+// get the path to the new composable folder
+const composableFolderPath = path.join(composablesPath, composableName)
+
+// create the new composable folder
+fs.mkdirSync(composableFolderPath)
+
+// get the path to the new composable
+const composablePath = path.join(composableFolderPath, `index.ts`)
+const composableTemplate = `import { computed, unref } from "vue"
+import type { Ref } from "vue"
+
+export function useSomething(value: Ref<number> | number) {
+  return computed(() => unref(value) * 2)
+}
+`
+
+// create the new composable
+fs.writeFileSync(composablePath, composableTemplate)
+
+// get the path to the new composable test
+const composableTestPath = path.join(composableFolderPath, `index.spec.ts`)
+const composableTestTemplate = `import { ref } from "vue"
+import { useSomething } from "./"
+
+describe("useSomething", () => {
+  it("should work", () => {
+    const input = ref(1)
+    const result = useSomething(input)
+    expect(result.value).toBe(2)
+  })
+})
+`
+
+// create the new composable test
+fs.writeFileSync(composableTestPath, composableTestTemplate)
