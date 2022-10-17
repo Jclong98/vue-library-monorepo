381b9b6b04614db782202d49178665eb8ffdd241
Jacob Long <jclong98@gmail.com>
2022-10-17 16:12:03 -0700

    feat: remove auto import from ui package

--- packages/ui/src/composables/useDouble/index.ts
+++ packages/ui/src/composables/useDouble/index.ts
@@ -1,3 +1,4 @@
+import { computed, unref } from 'vue'
 import type { MaybeRef } from '@vueuse/core'
 
 export function useDouble(value: MaybeRef<number>) {
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,7 +2,6 @@
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,10 +12,6 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    AutoImport({
-      imports: ['vue', 'vue-router', '@vueuse/core'],
-      dts: 'src/auto-imports.d.ts',
-    }),
   ],
   build: {
     lib: {
