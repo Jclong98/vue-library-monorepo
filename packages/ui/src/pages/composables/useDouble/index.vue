0fb7b5158d6ad43e95cbbaea7699b457c0669466
Jacob Long <jclong98@gmail.com>
2022-10-29 14:07:46 -0700

    feat: reintroduce composable example in ui library

---
+++ packages/ui/src/pages/composables/useDouble/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useDouble } from '@/composables'
+
+const count = ref(0)
+const doubleCount = useDouble(count)
+</script>
+
+<template>
+  <div>
+    <h1>useDouble</h1>
+
+    <label>
+      Number: <input v-model="count" type="number">
+    </label>
+
+    <pre>{{ { count, doubleCount } }}</pre>
+  </div>
+</template>
